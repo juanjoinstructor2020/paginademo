84af985b235f8611b5f0cda0ee8e979c18a07c72
juanjoinstructor2020 <67698829+juanjoinstructor2020@users.noreply.github.com>
2020-07-01 09:55:48 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+//DESARROLLAR EL CODIGO DE LOS EJERCICIOS  PROPUESTOS MEDIANTE
+//FUNCIONES
+
+//desarrollo 01
+function mostrar(){
+    alert("demo");
+}
+
+
+
+
+
+//desarrollo 02
+
+
+
+
+
+
+//desarrollo 03
